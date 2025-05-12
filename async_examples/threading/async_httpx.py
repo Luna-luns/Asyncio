f7d943992fd7bebb3c8bd3a5e0e5545043d710bd
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-12 15:26:53 +0300

    Update files

---
+++ async_examples/threading/async_httpx.py
@@ -0,0 +1,17 @@
+# Синхронное чтение кода состояния
+
+import time
+import httpx
+
+
+def read_example() -> None:
+    response = httpx.get('https://example.com')
+    print('Status code:', response.status_code)
+
+
+sync_start = time.perf_counter()
+read_example() # операция блокирующая
+read_example()
+sync_end = time.perf_counter()
+
+print(f"Выполнение кода заняло: {sync_end - sync_start:.4f} с.")
