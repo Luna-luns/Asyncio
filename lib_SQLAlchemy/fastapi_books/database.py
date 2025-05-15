c4744df7e1c623f01d7d12447257dda809fa2fbd
estrunnikova <estrunnikova@team.amocrm.com>
2025-05-15 23:20:54 +0300

    small fixes

--- lib_SQLAlchemy/fastapi_books/database.py
+++ lib_SQLAlchemy/fastapi_books/database.py
@@ -1,27 +1,42 @@
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession,async_sessionmaker
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession,async_sessionmaker
     
-DATABASE_URL_ASYNC = "sqlite+aiosqlite:///./a_books.db"
+# DATABASE_URL_ASYNC = "sqlite+aiosqlite:///./a_books.db"
+# DATABASE_URL = "sqlite:///./books.db"
+#
+# # engine = create_engine(DATABASE_URL)
+# async_engine = create_async_engine(DATABASE_URL_ASYNC, echo=False)
+#
+# # session_sync = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# session_async = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+# Base = declarative_base()
+#
+# async def get_db():
+#     db = session_async()
+#     try:
+#         yield db
+#     finally:
+#         await db.close()
+#
+#
+# # async def get_db_async():
+# #     async with session_async() as session:
+# #         query = select(Book).filter(**kwargs)
+# #         await session.execute(query)
+
 DATABASE_URL = "sqlite:///./books.db"
-
-# engine = create_engine(DATABASE_URL)
-async_engine = create_async_engine(DATABASE_URL_ASYNC, echo=False)
-
-# session_sync = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session_async = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-async def get_db():
-    db = session_async()
+
+def get_db():
+    db = SessionLocal()
     try:
         yield db
     finally:
-        await db.close()
+        db.close()
 
 
-# async def get_db_async():
-#     async with session_async() as session:
-#         query = select(Book).filter(**kwargs)
-#         await session.execute(query)
 
--- lib_SQLAlchemy/fastapi_books/main.py
+++ lib_SQLAlchemy/fastapi_books/main.py
@@ -1,5 +1,16 @@
-# from database import engine
-from database import async_engine
+from database import engine
+# from database import async_engine
+from fastapi import FastAPI
+from routers.books import router as books_router
 
-# print(engine.connect())
-print(await async_engine.connect())
+
+app = FastAPI()
+app.include_router(books_router, prefix="/books", tags=["books"])
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Books API"}
+
+print(engine.connect())
+# print(await async_engine.connect())
